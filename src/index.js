614818910fdc9be578e1534e68900ab28e171e46
theobittencourt <o.theobittencourt@gmail.com>
2024-08-25 23:18:07 -0300

    adjustments for gitpages

--- src/index.js
+++ src/index.js
@@ -4,12 +4,20 @@ import './index.css';
 import App from './App';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 import { gsap } from 'gsap';
+import { BrowserRouter } from 'react-router-dom';
 
 gsap.registerPlugin(ScrollTrigger);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
 
